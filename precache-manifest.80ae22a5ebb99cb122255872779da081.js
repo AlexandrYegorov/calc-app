32bf3dad71854fef8289c286f3f741ff3fbf3a9e
Alexandr <you@example.com>
2020-02-22 09:44:52 -0800

    Updates

---
+++ precache-manifest.80ae22a5ebb99cb122255872779da081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129c4a8f89a1697f0b6ee15c6fa989ee",
+    "url": "/calc-app/index.html"
+  },
+  {
+    "revision": "a5a38718fbffe9f9a0f9",
+    "url": "/calc-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aca82bfa409cb19c1076",
+    "url": "/calc-app/static/js/2.6924ed70.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calc-app/static/js/2.6924ed70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a38718fbffe9f9a0f9",
+    "url": "/calc-app/static/js/main.8b985626.chunk.js"
+  },
+  {
+    "revision": "bb968bf07d89d811cb33",
+    "url": "/calc-app/static/js/runtime-main.ffe2f0f0.js"
+  }
+]);
